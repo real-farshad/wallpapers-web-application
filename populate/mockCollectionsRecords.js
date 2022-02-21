5ad52f76016e479dce59c9752ac48acd923c367e
m0hammadr3za <mohammad.reza.gholami.dev@gmail.com>
2022-02-21 23:18:56 +0330

    populate database script added

---
+++ populate/mockCollectionsRecords.js
@@ -0,0 +1,39 @@
+const minutesInMs = 60 * 1000;
+
+module.exports = [
+    {
+        collection: "Nulla Congue Commodo",
+        wallpaper: "Sollicitudin Volutpat Velit Nec",
+        createdAt: new Date("8-28-2021").getTime() + minutesInMs,
+    },
+
+    {
+        collection: "Sed Mollis Libero Metus",
+        wallpaper: "Hendrerit Libero Morbi Quis",
+        createdAt: new Date("11-15-2021").getTime() + minutesInMs,
+    },
+
+    {
+        collection: "Facilisis Felis Nunc Quis",
+        wallpaper: "Odio Sapien Suspendisse Iaculis",
+        createdAt: new Date("11-1-2021").getTime() + minutesInMs,
+    },
+
+    {
+        collection: "Dolor Arcu Tristique Orci",
+        wallpaper: "Euismod Velit Sed Tortor",
+        createdAt: new Date("10-19-2021").getTime() + minutesInMs,
+    },
+
+    {
+        collection: "Lacus Tincidunt Commodo",
+        wallpaper: "Justo Nulla Porttitor Eleifend",
+        createdAt: new Date("10-4-2021").getTime() + minutesInMs,
+    },
+
+    {
+        collection: "Maecenas Molestie Erat",
+        wallpaper: "Suscipit Finibus Dolor Lacinia",
+        createdAt: new Date("10-5-2021").getTime() + minutesInMs,
+    },
+];
