3b0c41c0c8d3cd6c5276cb4dd587c5a0d42b53a8
Farshad <m0hammadr3za.dev@gmail.com>
2025-04-15 17:18:14 +0330

    ♻️ refactor signin and signup

--- client/src/components/GoogleSignIn.tsx
+++ client/src/components/GoogleSignIn.tsx
@@ -1,26 +1,19 @@
 import "../styles/GoogleSignIn.scss";
 
 function GoogleSignIn() {
-    function handleClickOnSignInBtn() {
-        let url = "/api/auth/google";
+  function handleClickOnSignInBtn() {
+    const baseURL = process.env.REACT_APP_SERVER_URL;
+    let url = `${baseURL}/api/auth/google`;
+    window.open(url, "_self");
+  }
 
-        if (process.env.NODE_ENV === "development") {
-            url = `http://localhost:${process.env.REACT_APP_SERVER_PORT}` + url;
-        }
-
-        window.open(url, "_self");
-    }
-
-    return (
-        <div className="google-sign-in">
-            <button
-                className="google-sign-in__btn"
-                onClick={handleClickOnSignInBtn}
-            >
-                Sign In With Google
-            </button>
-        </div>
-    );
+  return (
+    <div className="google-sign-in">
+      <button className="google-sign-in__btn" onClick={handleClickOnSignInBtn}>
+        Sign In With Google
+      </button>
+    </div>
+  );
 }
 
 export default GoogleSignIn;
--- client/src/components/GoogleSignUp.tsx
+++ client/src/components/GoogleSignUp.tsx
@@ -1,37 +1,30 @@
 import "../styles/GoogleSignUp.scss";
 
 function GoogleSignUp() {
-    function handleClickOnSignUpBtn() {
-        let url = "/api/auth/google";
+  function handleClickOnSignUpBtn() {
+    const baseURL = process.env.REACT_APP_SERVER_URL;
+    let url = `${baseURL}/api/auth/google`;
+    window.open(url, "_self");
+  }
 
-        if (process.env.NODE_ENV === "development") {
-            url = `http://localhost:${process.env.REACT_APP_SERVER_PORT}` + url;
-        }
+  return (
+    <div className="google-sign-up">
+      <h1 className="google-sign-up__title">
+        Sign Up With <br className="google-sign-up__title-break" />
+        Your Google Account
+      </h1>
 
-        window.open(url, "_self");
-    }
+      <p className="google-sign-up__description">
+        We recommend using this method, when you sign up with your google
+        account, you don't need to worry about forgetting your password and you
+        can sign in much faster and easier.
+      </p>
 
-    return (
-        <div className="google-sign-up">
-            <h1 className="google-sign-up__title">
-                Sign Up With <br className="google-sign-up__title-break" />
-                Your Google Account
-            </h1>
-
-            <p className="google-sign-up__description">
-                We recommend using this method, when you sign up with your
-                google account, you don't need to worry about forgetting your
-                password and you can sign in much faster and easier.
-            </p>
-
-            <button
-                className="google-sing-up__btn"
-                onClick={handleClickOnSignUpBtn}
-            >
-                Sign Up With Google
-            </button>
-        </div>
-    );
+      <button className="google-sing-up__btn" onClick={handleClickOnSignUpBtn}>
+        Sign Up With Google
+      </button>
+    </div>
+  );
 }
 
 export default GoogleSignUp;
